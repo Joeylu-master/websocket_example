a3004146112ad2897eb027252b8fb25cf1c90cf3
lumia <lumia@lumiadeMacBook-Pro.local>
2019-03-20 08:08:25 +0800

    checkin pytest for NotificationConsumer

--- foo/tests.py
+++ foo/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+import pytest
+from channels.testing import HttpCommunicator
 
-# Create your tests here.
+from foo.consumers import NotificationConsumer
+from foo.models import Foo, update_foo
+
+
+@pytest.mark.asyncio
+async def test_my_consumer():
+    communicator = HttpCommunicator(NotificationConsumer, "GET", "/test/")
+    response = await communicator.get_response()
+    assert response["body"] == b"test response"
+    assert response["status"] == 200
+    connected, _ = await communicator.connect()
+    assert connected
+
+    await communicator.send_json_to({
+        "model": "as.Appropriate",
+        "id": str(Foo.id),
+    })
+    assert await communicator.receive_nothing()
+
+    await update_foo()
+    response = await communicator.receive_json_from()
+    assert response == {
+        # ... whatever you expect
+    }
+
+    await communicator.disconnect()
